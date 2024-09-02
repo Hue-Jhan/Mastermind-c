6e545bb6b80cc3bd245e5f3d437add5d1ef91e58
Hue <75088696+Hue-Jhan@users.noreply.github.com>
2024-09-02 15:45:23 +0200

    Create Globali.h

---
+++ codice/source/Globali.h
@@ -0,0 +1,13 @@
+
+#ifndef GLOBALI_H_
+#define GLOBALI_H_
+
+#define datapath "C://Users//UTENTE//Desktop//partita.txt"
+#define dataPathGuida "C://Users//UTENTE//Desktop//guida.txt"
+#define RIGHE_SCHERMO 25
+#define COLONNE_SCHERMO 80
+#define MAXLUNCODICE 6
+#define MAXSIMBOLI 10
+#define MAXTENTATIVI 8
+
+#endif
