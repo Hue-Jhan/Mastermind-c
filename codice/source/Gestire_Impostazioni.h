e301cf7f108d7ec98e0afb902a8a4515f9170225
Hue <75088696+Hue-Jhan@users.noreply.github.com>
2024-09-02 15:52:48 +0200

    Create Gestire_Impostazioni.h

---
+++ codice/source/Gestire_Impostazioni.h
@@ -0,0 +1,10 @@
+
+#ifndef GESTIRE_IMPOSTAZIONI_H_
+#define GESTIRE_IMPOSTAZIONI_H_
+#include "Gestire_Tipi.h"
+
+void impostazioniDefault(Impostazioni*imp, int t, int l, int s, int d);
+void visualizzareImpostazioni(Impostazioni*imp);
+void modificareImpostazioni(Impostazioni*imp);
+
+#endif
